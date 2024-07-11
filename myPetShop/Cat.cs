ef272b36929b48eb0df40bf0c9b93b153e6abd51
OB <oliverbadger@aol.com>
2024-07-11 13:58:23 +0100

    Add project files.

---
+++ myPetShop/Cat.cs
@@ -0,0 +1,11 @@
+﻿namespace myPetShop;
+
+class Cat : Pet
+{
+
+    public Cat(int id, string colour, string gender, string size, int age, 
+        int numberOfLegs, string name, string description, 
+        int price) : base(id, colour, gender, size, age, numberOfLegs, name, description, price)
+    {
+    }
+}
---
+++ myPetShop/Dog.cs
@@ -0,0 +1,20 @@
+﻿namespace myPetShop;
+
+class Dog : Pet
+{
+    private bool _hasTail;
+    private string? _breed;
+    private bool _hasFur;
+
+    public Dog(int id, string colour, string gender, string breed, string size, int age, 
+        int numberOfLegs, string name, string description, int price, 
+        bool hasTail, bool hasFur) : base(id, colour, gender, size, age, numberOfLegs, name, description, price)
+    {
+        _hasTail = hasTail;
+        _breed = breed;
+        _hasFur = hasFur;
+    }
+
+    public override void Sleep() => Console.WriteLine("zzz woof woof zzz!");
+    public void Play() => Console.WriteLine("Im playing fetch!");
+}
